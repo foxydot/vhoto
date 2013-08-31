57d79979e93cd7d5b7cf6a5ef874fd96c86da596
cmos <catherine@madsciencedept.com>
2013-08-31 11:50:23 -0400

    ass modal login, change to js files

--- wp-content/plugins/msd-vhotopic/lib/js/msd-contest-vote.php
+++ wp-content/plugins/msd-vhotopic/lib/js/msd-contest-vote.php
@@ -4,13 +4,18 @@ jQuery(document).ready(function($) {
 		var entry = $(this).parents('.entry-item');
 		var vote_id = $(this).attr('id');
 		//do ajax call to post the vote to the correct post id AND blog id.
-		$.post('<?php print plugins_url('/vote.php', dirname(__FILE__)); ?>',{'action':'vote','vote_id':vote_id}, function(data) {
-				var data_array = data.split('|');
-				if(data_array[0]>0){
-					  entry.find('.total_votes').html(data_array[0]);
-					  entry.find('.vote-button').html('VOTE ADDED!').removeClass('vote-button').css('cursor','default');
-				} 
-			});
+		if($(this).hasClass('vote-button')){
+			$.post('<?php print plugins_url('/vote.php', dirname(__FILE__)); ?>',{'action':'vote','vote_id':vote_id}, function(data) {
+					var data_array = data.split('|');
+					if(data_array[0]>0){
+						  entry.find('.total_votes').html(data_array[0]);
+						  entry.find('.vote-button').html('VOTE ADDED!');
+						  $('.vote-button').removeClass('vote-button').addClass('voted').css('cursor','default');
+					} 
+				});
+		} else {
+			alert('You have already voted in this contest.');
+		}
 		//reload the page for now.
 	});
 });
